d8bbd6211bfec401cc562004cf35ec9c06e4232c
in0k <val@kreit.ru>
2015-01-13 17:21:47 +0500

    work continues
    почистил, и все сдвинулось с мёртвой точки

--- in0k_lazExt_AFC.pas
+++ in0k_lazExt_AFC.pas
@@ -13,6 +13,7 @@ interface
 
 uses {$ifOpt D+} in0k_lazExt_AFC_wndDBG, {$endIf}
     Classes, sysutils,
+    AFC_Config_Object,
     AFC_Config_Editor,
      AFC_Config_Handle,
     // in0k_lazExt_AFC,
@@ -29,12 +30,22 @@ type
 
  tIn0k_lazExt_AFC=class
   strict private
-   _lastProc:tIn0k_lazExt_AFC_synEdit; //< последний ОБРАБОТАННЫЙ
+   _lastProc:TSourceEditorInterface; //< последний ОБРАБОТАННЫЙ
+  protected //< ВСЯ СУТЬ этого "дополнения"
+    function  _ideLaz_get_ActvEDT:TSourceEditorInterface;
+    function  _ideLaz_get_CodeBUF(const Editor:TSourceEditorInterface):TCodeBuffer;
+    function  _ideLaz_get_cnfgOBJ(const Editor:TSourceEditorInterface):pAFC_Config_Object;
+    function  _ideLaz_get_afcEDIT(const Editor:TSourceEditorInterface):tIn0k_lazExt_AFC_synEdit;
+
+
+
+    //function  _ideLaz_getActiveEditor_fileName:string;
   protected //< ВСЯ СУТЬ этого "дополнения"
     function  _perform_AFC_getActiveEditor(out CodeBuffer:TCodeBuffer):tIn0k_lazExt_AFC_synEdit;
+    function  _perform_AFC_execute(const afcEdit:tIn0k_lazExt_AFC_synEdit; const cnfgOBJ:pAFC_Config_Object):boolean;
     procedure _perform_AFC;
   protected //< СОБЫТИЯ
-    procedure _ideEvent_srcEditorActivate(Sender: TObject);
+    procedure _ideEvent_semEditorActivate(Sender: TObject);
     procedure _ideEvent_closeIDE(Sender: TObject);
   protected
    _SubScriber_:pointer;
@@ -42,24 +53,12 @@ type
   protected //< и их РЕГИСТРАЦИЯ
     procedure _ideEvents_Register;
     procedure _ideEvents_unRegister;
-  protected //< настройки
-   {_nameList:tStrings; //< список "Имен"
-   _lazExtON:boolean;  //< мы вообще работаем
-   _fold_ALL:boolean;  //< сворачивать ВСЕ
-   _fold_LST:boolean;  //< сворачивать ВСЕ
-   _fold_HFC:boolean;  //< сворачивать Hint From Comment
-    procedure _nameList_set(const names:tStrings);
-    procedure _nameList_get(const names:tStrings); }
   protected
-    //procedure _settings_Load;
-    //procedure _settings_Save;
-    //procedure _settings_toDefault;
+    function  _DEBUG_get_ActiveEditor_Name:string;
+    procedure  DEBUG(const msgType,msgText:string);
   public
     constructor Create;
     destructor DESTROY; override;
-  public
-    //procedure SaveSettings;
-    //procedure SaveDefSettings;
   end;
 
 procedure In0k_lazExt_AFC__CREATE;
@@ -84,23 +83,29 @@ end;
 
 //------------------------------------------------------------------------------
 
+
+
 {%region --- события IDE Lazarus ---------------------------------- /fold}
 
 procedure tIn0k_lazExt_AFC._ideEvent_closeIDE(Sender: TObject);
-begin
-    {*2> причины использования `_closeIDE` /fold
-        не понятно по какому принципу необходимо отписываться от событий.
-        но без этого Lazarus завершается с утечкой памяти или вообще падает.
-    <*2}
+begin {***> причины использования `_closeIDE`
+            не понятно по какому принципу необходимо отписываться от событий.
+            но без этого Lazarus завершается с утечкой памяти или вообще падает.
+      }
+    {$ifOpt D+}
+    DEBUG('ideEvent','closeIDE');
+    {$endIf}
    _ideEvents_unRegister;
 end;
 
-procedure tIn0k_lazExt_AFC._ideEvent_srcEditorActivate(Sender: TObject);
-begin
-    {*3> рабочее Событие. /fold
-        При АКТИВАЦИИ вкладки редактора исходного кода выполняем "ДОБАВКУ"
-    <*3}
-    {if _lazExtON then} _perform_AFC;
+procedure tIn0k_lazExt_AFC._ideEvent_semEditorActivate(Sender: TObject);
+begin {***> рабочее Событие.
+            При АКТИВАЦИИ вкладки редактора исходного кода выполняем "ДОБАВКУ"
+      }
+    {$ifOpt D+}
+    DEBUG('ideEvent','semEditorActivate: '+_DEBUG_get_ActiveEditor_Name);
+    {$endIf}
+   _perform_AFC;
 end;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -110,20 +115,21 @@ const cIn0k_lazExt_AFNC_registerEvent=semEditorActivate;
 procedure tIn0k_lazExt_AFC._ideEvents_register;
 begin
     LazarusIDE.AddHandlerOnIDEClose(@_ideEvent_closeIDE);
-    SourceEditorManagerIntf.RegisterChangeEvent(cIn0k_lazExt_AFNC_registerEvent, @_ideEvent_srcEditorActivate);
+    SourceEditorManagerIntf.RegisterChangeEvent(cIn0k_lazExt_AFNC_registerEvent, @_ideEvent_semEditorActivate);
 end;
 
 procedure tIn0k_lazExt_AFC._ideEvents_unRegister;
 begin
     LazarusIDE.RemoveHandlerOnIDEClose(@_ideEvent_closeIDE);
-    SourceEditorManagerIntf.UnRegisterChangeEvent(cIn0k_lazExt_AFNC_registerEvent, @_ideEvent_srcEditorActivate);
+    SourceEditorManagerIntf.UnRegisterChangeEvent(cIn0k_lazExt_AFNC_registerEvent, @_ideEvent_semEditorActivate);
 end;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 procedure tIn0k_lazExt_AFC._SubScriber_Register;
 begin
    _SubScriber_:=CbSFP_SubScriber__REGISTER(tAFC_Config_Handle,tAFC_Config_Editor);
 end;
-
 
 {%endregion}
 
@@ -132,77 +138,179 @@ end;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+function tIn0k_lazExt_AFC._ideLaz_get_ActvEDT:TSourceEditorInterface;
+begin
+    result:=SourceEditorManagerIntf.ActiveEditor;
+    {$ifOpt D+}
+    if not Assigned(result) then DEBUG('ER','ActiveEditor is NULL');
+    {$endIf}
+end;
+
+function tIn0k_lazExt_AFC._ideLaz_get_afcEDIT(const Editor:TSourceEditorInterface):tIn0k_lazExt_AFC_synEdit;
+begin
+    result:=tIn0k_lazExt_AFC_synEdit(Editor.EditorControl);
+    {$ifOpt D+}
+    if not Assigned(result) then DEBUG('ER','AFC_synEdit is NULL');
+    {$endIf}
+end;
+
+function tIn0k_lazExt_AFC._ideLaz_get_CodeBUF(const Editor:TSourceEditorInterface):TCodeBuffer;
+begin
+    result:=TCodeBuffer(Editor.CodeToolsBuffer);
+    {$ifOpt D+}
+    if not Assigned(result) then DEBUG('ER','CodeBuffer is NULL');
+    {$endIf}
+end;
+
+function tIn0k_lazExt_AFC._ideLaz_get_cnfgOBJ(const Editor:TSourceEditorInterface):pAFC_Config_Object;
+begin
+    result:=CbSFP_SubScriber__cnfg_OBJ(_SubScriber_,Editor.FileName);
+    {$ifOpt D+}
+    if not Assigned(result) then DEBUG('ER','cnfgOBJECT is NULL');
+    {$endIf}
+end;
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 function tIn0k_lazExt_AFC._perform_AFC_getActiveEditor(out CodeBuffer:TCodeBuffer):tIn0k_lazExt_AFC_synEdit;
 begin // вытягиваем текущий активный synEdit
-    CodeBuffer     :=nil;
-    pointer(result):=SourceEditorManagerIntf.ActiveEditor;
+  (*  CodeBuffer     :=nil;
+    pointer(result):=_ideLaz_getActiveEditor{SourceEditorManagerIntf.ActiveEditor};
     if Assigned(result) then begin
         {$ifOpt D+}
-       _dbgLOG_('getActiveEditor found:'+TSourceEditorInterface(pointer(result)).FileName);
+        DEBUG('ok','ActiveEditor found : '+'@'+IntToHex( PtrUInt(pointer(result)), sizeOf(pointer)*2 ));
         {$endIf}
         CodeBuffer:=TCodeBuffer(TSourceEditorInterface(pointer(result)).CodeToolsBuffer);
         if assigned(CodeBuffer)
-        then result:=tIn0k_lazExt_AFC_synEdit(TCustomSynEdit(TSourceEditorInterface(pointer(result)).EditorControl))
+        then begin
+            result:=tIn0k_lazExt_AFC_synEdit(TCustomSynEdit(TSourceEditorInterface(pointer(result)).EditorControl));
+            {$ifOpt D+}
+            DEBUG('ok','CodeBuffer found : '+'@'+IntToHex( PtrUInt(pointer(CodeBuffer)), sizeOf(pointer)*2 ));
+            {$endIf}
+        end
         else begin
             result:=nil;
             {$ifOpt D+}
-           _dbgLOG_('CodeBuffer is NULL');
+            DEBUG('ER','CodeBuffer is NULL');
             {$endIf}
         end
     end
     {$ifOpt D+}
     else begin
-        _dbgLOG_('SourceEditorManagerIntf.ActiveEditor is NULL');
+        DEBUG('ER','ActiveEditor is NULL');
     end
+    {$endIf}
+    *)
+end;
+
+
+function tIn0k_lazExt_AFC._perform_AFC_execute(const afcEdit:tIn0k_lazExt_AFC_synEdit; const cnfgOBJ:pAFC_Config_Object):boolean;
+begin
+    {$ifOpt D+}
+    DEBUG('Execute ','>>>>> START ===== '+'afcSynEDIT->@'+IntToHex( PtrUInt(pointer(afcEdit)), sizeOf(pointer)*2 )+' cnfgOBJ->@'+IntToHex( PtrUInt(pointer(cnfgOBJ)), sizeOf(pointer)*2 ));
+    {$endIf}
+    //--------------------------------------------------------------------------
+    result:=TRUE;
+
+    {if not _fold_ALL then begin
+        if not _fold_HFC
+
+        //then CodeBuf:=nil; //< использовать НЕ будем
+        then ActvEdt.foldComments_Name(_workList,nil)
+        else ActvEdt.foldComments_Name(_workList,CodeBuf)
+    end
+    else ActvEdt.foldComments_ALL;   }
+    afcEdit.foldComments_ALL;
+    //--------------------------------------------------------------------------
+    {$ifOpt D+}
+    DEBUG('Execute ','<<<<<  END  =====');
     {$endIf}
 end;
 
+
 procedure tIn0k_lazExt_AFC._perform_AFC;
-var tmpEdit:tIn0k_lazExt_AFC_synEdit;
+var ActvEdt:TSourceEditorInterface;
+    cnfgOBJ:pAFC_Config_Object;
     CodeBuf:TCodeBuffer;
-
-    cnf:pointer;
-
-
+    afcEdit:tIn0k_lazExt_AFC_synEdit;
 begin
-    tmpEdit:=_perform_AFC_getActiveEditor(CodeBuf);
-    if Assigned(tmpEdit) and Assigned(CodeBuf) then begin
-        {*1> причины использования `_lastProcessed` /fold
-            механизм с `_lastProcessed` приходится использовать из-за того, что
-            при переключение "Вкладок Редактора Исходного Кода" вызов данного
-            события происходит аж 3(три) раза.
-            Почему так происходит - повод для дальнейших разобирательств.
-        <*1}
-        if _lastProc<>tmpEdit then begin
-            {$ifOpt D+}
-           _dbgLOG_(' ==== START for file :'+CodeBuf.Filename);
-            {$endIf}
-
-            cnf:=CbSFP_SubScriber__cnfg_OBJ(_SubScriber_,CodeBuf.Filename);
-
-            {$ifOpt D+}
-            if cnf<>nil then
-           _dbgLOG_(' ==== CNFG FIND :'+IntToHex(int64(cnf),8));
-            {$endIf}
-
-            {if not _fold_ALL then begin
-                if not _fold_HFC
-
-                //then CodeBuf:=nil; //< использовать НЕ будем
-                then tmpEdit.foldComments_Name(_workList,nil)
-                else tmpEdit.foldComments_Name(_workList,CodeBuf)
+    ActvEdt:=_ideLaz_get_ActvEDT;//(CodeBuf);
+    if Assigned(ActvEdt) then begin
+        if ActvEdt<>_lastProc then begin
+            {*1> причины использования `_lastProc`
+                механизм с `_lastProcessed` приходится использовать из-за того, что
+                при переключение "Вкладок Редактора Исходного Кода" вызов данного
+                события происходит аж 3(три) раза.
+                Почему так происходит - повод для дальнейших разобирательств.
+                -----
+                еще это событие происходит КОГДА идет навигация (прыжки по файлу)
+                -----
+                Используем только ПЕРВЫЙ вход
+            }
+            {todo: проверка на обрабатывание ТОЛЬКО в слючае ОТКРЫТИЯ}
+            afcEdit:=_ideLaz_get_afcEDIT(ActvEdt);
+            if Assigned(afcEdit) then begin
+                cnfgOBJ:=_ideLaz_get_cnfgOBJ(ActvEdt);
+                if Assigned(cnfgOBJ) then begin
+                    if _perform_AFC_execute(afcEdit,cnfgOBJ) then begin //< тут работа
+                       _lastProc:=ActvEdt;
+                    end
+                    else begin
+                       _lastProc:=nil;
+                        {$ifOpt D+}
+                        DEBUG('SKIP','EXECUTE FAILs');
+                        {$endIf}
+                    end;
+                end
+                else begin
+                   _lastProc:=nil;
+                    {$ifOpt D+}
+                    DEBUG('SKIP','cnfgOBJ not Ready');
+                    {$endIf}
+                end;
             end
-            else tmpEdit.foldComments_ALL;   }
+            else begin
+               _lastProc:=nil;
+                {$ifOpt D+}
+                DEBUG('SKIP','afcEdit not Ready');
+                {$endIf}
+            end;
+        end
+        else begin
             {$ifOpt D+}
-           _dbgLOG_(' ====  END  =========:');
+            DEBUG('SKIP','already processed');
             {$endIf}
-           _lastProc:=tmpEdit;
         end;
     end
-    else _lastProc:=nil;
+    else begin
+       _lastProc:=nil;
+        {$ifOpt D+}
+        DEBUG('SKIP','IDE not Ready');
+        {$endIf}
+    end;
 end;
 
+
 {%endregion}
+
+function tIn0k_lazExt_AFC._DEBUG_get_ActiveEditor_Name:string;
+var tmp:TSourceEditorInterface;
+begin
+    tmp:=_ideLaz_get_ActvEDT;
+    if not Assigned(tmp) then result:='ndf'
+    else begin
+        result:='ActiveEDT->@'+IntToHex( PtrUInt(pointer(tmp)), sizeOf(pointer)*2 );
+        result:=result+' '+ExtractFileName(tmp.FileName);
+    end;
+end;
+
+procedure tIn0k_lazExt_AFC.DEBUG(const msgType,msgText:string);
+begin
+    CbSFP_SubScriber__DebugMSG(_SubScriber_,msgType,msgText);
+end;
+
 
 //==============================================================================
 var _In0k_lazExt_AFNC_:tIn0k_lazExt_AFC;
